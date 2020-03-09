c67189813bd0cf84e63ab659009297410e364144
alex-bobrik <sasha.bobrik@inbox.ru>
2020-03-09 22:02:25 +0300

    Update MapOptionService

--- src/Service/MapOptionService.php
+++ src/Service/MapOptionService.php
@@ -4,7 +4,22 @@
 namespace App\Service;
 
 
+use App\Entity\MapOption;
+use Doctrine\ORM\EntityManagerInterface;
+
 class MapOptionService
 {
+    private $em;
+
+    public function __construct(EntityManagerInterface $em)
+    {
+        $this->em = $em;
+    }
+
+    public function saveMapOption(MapOption $mapOption)
+    {
+        $this->em->persist($mapOption);
+        $this->em->flush();
+    }
 
 }
