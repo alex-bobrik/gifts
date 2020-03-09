665cc61c8415636774565b5583410cb3ca982185
alex-bobrik <sasha.bobrik@inbox.ru>
2020-03-09 21:53:32 +0300

    Add tag editing

--- src/Controller/TagController.php
+++ src/Controller/TagController.php
@@ -7,6 +7,7 @@ use App\Form\Item\TagType;
 use App\Service\TagService;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class TagController extends AbstractController
@@ -28,7 +29,7 @@ class TagController extends AbstractController
      * @Route("/admin/tags/new", name="admin_tag_new")
      * @param Request $request
      * @param TagService $tagService
-     * @return \Symfony\Component\HttpFoundation\Response
+     * @return Response
      */
     public function newTag(Request $request, TagService $tagService)
     {
@@ -51,11 +52,28 @@ class TagController extends AbstractController
 
     /**
      * @Route("/admin/tags/edit/{id}", name="admin_tag_edit")
+     * @param Request $request
+     * @param TagService $tagService
+     * @param int $id
+     * @return Response
      */
-    public function editTag()
+    public function editTag(Request $request, TagService $tagService, int $id)
     {
-        return $this->render('tag/index.html.twig', [
+        $tag = $this->getDoctrine()->getRepository(Tag::class)->find($id);
+
+        $form = $this->createForm(TagType::class, $tag);
+
+        $form->handleRequest($request);
+        if ($form->isSubmitted()) {
+            $tag = $form->getData();
+            $tagService->saveTag($tag);
+
+            return $this->redirectToRoute('admin_tags');
+        }
+
+        return $this->render('tag/edit.html.twig', [
             'controller_name' => 'TagController',
+            'form' => $form->createView(),
         ]);
     }
 
