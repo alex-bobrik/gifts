57b7f56a6458b5a34c73fa9bc17febc85df88ca3
alex-bobrik <sasha.bobrik@inbox.ru>
2020-03-10 12:18:11 +0300

    Add FindController

---
+++ src/Controller/FindController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class FindController extends AbstractController
+{
+    /**
+     * @Route("/find", name="find_items")
+     */
+    public function index()
+    {
+
+
+        return $this->render('find/index.html.twig', [
+            'controller_name' => 'FindController',
+        ]);
+    }
+}
