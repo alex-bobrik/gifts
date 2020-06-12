e1414eb57ec3c4b8ca0b8a39b3db305bd34457ec
alex-bobrik <sasha.bobrik@inbox.ru>
2020-06-13 01:26:53 +0300

    Add validation for order form

--- src/Form/OrderType.php
+++ src/Form/OrderType.php
@@ -8,6 +8,7 @@ use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\NumberType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
@@ -30,9 +31,10 @@ class OrderType extends AbstractType
                 'attr' => [
                     'class' => 'form-control',
                     'maxLength' => '50',
+                    'onkeypress' => 'return isNumberKey(event)',
                 ]
             ])
-            ->add('email', TextType::class, [
+            ->add('email', EmailType::class, [
                 'label' => 'Email',
                 'attr' => [
                     'class' => 'form-control',
